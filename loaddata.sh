2c41234c2ed239f7308aa87b8553436c003c93f2
Jamie Scott <jamie@redislabs.com>
2020-01-15 15:52:05 -0800

    Create loaddata.sh

---
+++ loaddata.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+redis-cli hset secret:users:1 gender m birthdate 22027 lname Stella fname Smith address 10932 Fake Street city Santa Clara state CA zip 95051 phone 555-555-5555 email Smith@domain.com ccn 5270 4267 6450 5555 ccn_expire 32542
+redis-cli hset secret:users:2 gender f birthdate 22028 lname Angelika fname Garcia address 10933 Fake Street city Santa Clara state CA zip 95052 phone 555-555-5556 email Garcia@domain.com ccn 5271 4267 6450 5555 ccn_expire 32543
+redis-cli hset secret:users:3 gender f birthdate 22029 lname Green fname Hunan address 10934 Fake Street city Santa Clara state CA zip 95053 phone 555-555-5557 email Hunan@domain.com ccn 5272 4267 6450 5555 ccn_expire 32544
+redis-cli hset secret:users:4 gender m birthdate 22030 lname Hilbert fname Sam address 10935 Fake Street city Santa Clara state CA zip 95054 phone 555-555-5558 email Sam@domain.com ccn 5273 4267 6450 5555 ccn_expire 32545
+redis-cli hset secret:users:5 gender m birthdate 22031 lname Carl fname Bob address 10936 Fake Street city Santa Clara state CA zip 95055 phone 555-555-5559 email Bob@domain.com ccn 5274 4267 6450 5555 ccn_expire 32546
+redis-cli hset secret:users:6 gender f birthdate 22032 lname Alfons fname Billy address 10937 Fake Street city Santa Clara state CA zip 95056 phone 555-555-5560 email Billy@domain.com ccn 5275 4267 6450 5555 ccn_expire 32547
+redis-cli hset secret:users:7 gender f birthdate 22033 lname Arnesto fname Kasey address 10938 Fake Street city Santa Clara state CA zip 95057 phone 555-555-5561 email Kasey@domain.com ccn 5276 4267 6450 5555 ccn_expire 32548
+redis-cli hset secret:users:8 gender m birthdate 22034 lname Conley fname Bart address 10939 Fake Street city Santa Clara state CA zip 95058 phone 555-555-5562 email Bart@domain.com ccn 5277 4267 6450 5555 ccn_expire 32549
+redis-cli hset secret:users:9 gender M birthdate 22035 lname Jackson fname Linda address 10940 Fake Street city Santa Clara state CA zip 95059 phone 555-555-5563 email Linda@domain.com ccn 5278 4267 6450 5555 ccn_expire 32550
+redis-cli hset secret:users:10 gender f birthdate 22036 lname Uri fname Linden address 10941 Fake Street city Santa Clara state CA zip 95060 phone 555-555-5564 email Linden@domain.com ccn 5279 4267 6450 5555 ccn_expire 32551
+redis-cli hset secret:users:11 gender f birthdate 22037 lname Watson fname Viking Man address 10942 Fake Street city Santa Clara state CA zip 95061 phone 555-555-5565 email Viking Man@domain.com ccn 5280 4267 6450 5555 ccn_expire 32552
+redis-cli hset secret:users:12 gender f birthdate 22038 lname Garrison fname Turkey address 10943 Fake Street city Santa Clara state CA zip 95062 phone 555-555-5566 email Turkey@domain.com ccn 5281 4267 6450 5555 ccn_expire 32553
+redis-cli hset secret:users:13 gender f birthdate 22039 lname Renfro fname Chicken address 10944 Fake Street city Santa Clara state CA zip 95063 phone 555-555-5567 email Chicken@domain.com ccn 5282 4267 6450 5555 ccn_expire 32554
+redis-cli hset secret:users:14 gender m birthdate 22040 lname Heard fname Food address 10945 Fake Street city Santa Clara state CA zip 95064 phone 555-555-5568 email Food@domain.com ccn 5283 4267 6450 5555 ccn_expire 32555
+redis-cli hset secret:users:15 gender m birthdate 22041 lname Reyes fname Yummy address 10946 Fake Street city Mountain View state CA zip 95065 phone 555-555-5569 email Yummy@domain.com ccn 5284 4267 6450 5555 ccn_expire 32556
+redis-cli hset secret:users:16 gender m birthdate 22042 lname Mark fname Nicole address 10947 Fake Street city Mountain View state CA zip 95066 phone 555-555-5570 email Nicole@domain.com ccn 5285 4267 6450 5555 ccn_expire 32557
+redis-cli hset secret:users:17 gender m birthdate 22043 lname Bob fname Akshitha address 10948 Fake Street city Mountain View state CA zip 95067 phone 555-555-5571 email Akshitha@domain.com ccn 5286 4267 6450 5555 ccn_expire 32558
+redis-cli hset secret:users:18 gender m birthdate 22044 lname George fname Kristen address 10949 Fake Street city Mountain View state CA zip 95068 phone 555-555-5572 email Kristen@domain.com ccn 5287 4267 6450 5555 ccn_expire 32559
+redis-cli hset secret:users:19 gender m birthdate 22045 lname Thrall fname Christine address 10950 Fake Street city Mountain View state CA zip 95069 phone 555-555-5573 email Christine@domain.com ccn 5288 4267 6450 5555 ccn_expire 32560
+redis-cli hset secret:users:20 gender f birthdate 22046 lname Bobathan fname Dean address 10951 Fake Street city Mountain View state CA zip 95070 phone 555-555-5574 email Dean@domain.com ccn 5289 4267 6450 5555 ccn_expire 32561
+redis-cli hset secret:users:21 gender f birthdate 22047 lname Morrison fname Nomom address 10952 Fake Street city Mountain View state CA zip 95071 phone 555-555-5575 email Nomom@domain.com ccn 5290 4267 6450 5555 ccn_expire 32562
+redis-cli hset secret:users:22 gender m birthdate 22048 lname Nick fname Parker address 10953 Fake Street city Mountain View state CA zip 95072 phone 555-555-5576 email Parker@domain.com ccn 5291 4267 6450 5555 ccn_expire 32563
+redis-cli hset secret:users:23 gender m birthdate 22049 lname Santana fname Blake address 10954 Fake Street city Mountain View state CA zip 95073 phone 555-555-5577 email Blake@domain.com ccn 5292 4267 6450 5555 ccn_expire 32564
+redis-cli hset secret:users:24 gender m birthdate 22050 lname Lauren fname Kyle address 10955 Fake Street city Mountain View state CA zip 95074 phone 555-555-5578 email Kyle@domain.com ccn 5293 4267 6450 5555 ccn_expire 32565
+redis-cli hset secret:users:25 gender f birthdate 22051 lname Jonathan fname Cassie address 10956 Fake Street city Oakland state CA zip 95075 phone 555-555-5579 email Cassie@domain.com ccn 5294 4267 6450 5555 ccn_expire 32566
+redis-cli hset secret:users:26 gender m birthdate 22052 lname Brian fname James address 10957 Fake Street city Oakland state CA zip 95076 phone 555-555-5580 email James@domain.com ccn 5295 4267 6450 5555 ccn_expire 32567
+redis-cli hset secret:users:27 gender f birthdate 22053 lname Brown fname Jamie address 10958 Fake Street city Oakland state CA zip 95077 phone 555-555-5581 email Jamie@domain.com ccn 5296 4267 6450 5555 ccn_expire 32568
+redis-cli hset secret:users:28 gender f birthdate 22054 lname Nice fname Teapot address 10959 Fake Street city Oakland state CA zip 95078 phone 555-555-5582 email Teapot@domain.com ccn 5297 4267 6450 5555 ccn_expire 32569
+redis-cli hset secret:users:29 gender f birthdate 22055 lname Name fname Redis address 10960 Fake Street city Oakland state CA zip 95079 phone 555-555-5583 email Redis@domain.com ccn 5298 4267 6450 5555 ccn_expire 32570
+redis-cli hset secret:users:30 gender f birthdate 22056 lname John fname Fun address 10961 Fake Street city Oakland state CA zip 95080 phone 555-555-5584 email Fun@domain.com ccn 5299 4267 6450 5555 ccn_expire 32571
+
+redis-cli set public:username:id:Stella 1
+redis-cli set public:username:id:Angelika 2
+redis-cli set public:username:id:Green 3
+redis-cli set public:username:id:Hilbert 4
+redis-cli set public:username:id:Carl 5
+redis-cli set public:username:id:Alfons 6
+redis-cli set public:username:id:Arnesto 7
+redis-cli set public:username:id:Conley 8
+redis-cli set public:username:id:Jackson 9
+redis-cli set public:username:id:Uri 10
+redis-cli set public:username:id:Watson 11
+redis-cli set public:username:id:Garrison 12
+redis-cli set public:username:id:Renfro 13
+redis-cli set public:username:id:Heard 14
+redis-cli set public:username:id:Reyes 15
+redis-cli set public:username:id:Mark 16
+redis-cli set public:username:id:Bob 17
+redis-cli set public:username:id:George 18
+redis-cli set public:username:id:Thrall 19
+redis-cli set public:username:id:Bobathan 20
+redis-cli set public:username:id:Morrison 21
+redis-cli set public:username:id:Nick 22
+redis-cli set public:username:id:Santana 23
+redis-cli set public:username:id:Lauren 24
+redis-cli set public:username:id:Jonathan 25
+redis-cli set public:username:id:Brian 26
+redis-cli set public:username:id:Brown 27
+redis-cli set public:username:id:Nice 28
+redis-cli set public:username:id:Name 29
+redis-cli set public:username:id:John 30
+
+redis-cli set topsecret:1:passwd password
+redis-cli set topsecret:2:passwd password
+redis-cli set topsecret:3:passwd password
+redis-cli set topsecret:4:passwd password
+redis-cli set topsecret:5:passwd password
+redis-cli set topsecret:6:passwd password
+redis-cli set topsecret:7:passwd password
+redis-cli set topsecret:8:passwd password
+redis-cli set topsecret:9:passwd password
+redis-cli set topsecret:10:passwd password
+redis-cli set topsecret:11:passwd password
+redis-cli set topsecret:12:passwd password
+redis-cli set topsecret:13:passwd password
+redis-cli set topsecret:14:passwd password
+redis-cli set topsecret:15:passwd password
+redis-cli set topsecret:16:passwd password
+redis-cli set topsecret:17:passwd password
+redis-cli set topsecret:18:passwd password
+redis-cli set topsecret:19:passwd password
+redis-cli set topsecret:20:passwd password
+redis-cli set topsecret:21:passwd password
+redis-cli set topsecret:22:passwd password
+redis-cli set topsecret:23:passwd password
+redis-cli set topsecret:24:passwd password
+redis-cli set topsecret:25:passwd password
+redis-cli set topsecret:26:passwd password
+redis-cli set topsecret:27:passwd password
+redis-cli set topsecret:28:passwd password
+redis-cli set topsecret:29:passwd password
+redis-cli set topsecret:30:passwd password
+
+redis-cli set public:inventory:chairs:count 15
+redis-cli set public:inventory:couches:count 27
+redis-cli set public:inventory:firewood:count 2002
+redis-cli set public:inventory:dressers:count 30
+redis-cli set public:inventory:beds:count 55
+redis-cli set public:inventory:forks:count 10000
+redis-cli set public:inventory:desks:count 55
+redis-cli set public:inventory:loveseat:count 15
+redis-cli set public:inventory:fireplace:count 27
+redis-cli set public:inventory:oven:count 2002
+redis-cli set public:inventory:microwave:count 30
+redis-cli set public:inventory:table:count 55
+redis-cli set public:inventory:tiles:count 10000
+redis-cli set public:inventory:plates:count 55
+
+redis-cli set public:inventory:chairs:price 15
+redis-cli set public:inventory:couches 27
+redis-cli set public:inventory:firewood 2002
+redis-cli set public:inventory:dressers 30
+redis-cli set public:inventory:beds 55
+redis-cli set public:inventory:forks 10000
+redis-cli set public:inventory:desks 55
+redis-cli set public:inventory:loveseat 15
+redis-cli set public:inventory:fireplace 27
+redis-cli set public:inventory:oven 2002
+redis-cli set public:inventory:microwave 30
+redis-cli set public:inventory:table 55
+redis-cli set public:inventory:tiles 10000
+redis-cli set public:inventory:plates 55
+
+redis-cli hset public:order:1 item table user 2 
+redis-cli hset public:order:2 item beds user 3
+redis-cli hset public:order:3 item tiles user 5
+redis-cli hset public:order:4 item firewood user 11
+redis-cli hset public:order:5 item couches user 22
+redis-cli hset public:order:6 item loveseat user 21
+redis-cli hset public:order:7 item oven user 23 
+
+redis-cli acl setuser serviceacct2 on +set +get ~topsecret:* \>password
+redis-cli acl setuser serviceacct on \>password2 +hset +hget +hgetall ~secret:*
+redis-cli acl setuser serviceacct3 on \>password3 +set +get +hset +hget +hgetall ~public:*
+redis-cli acl setuser user1 on \>password1 +set +hget ~public:*
+redis-cli acl setuser admin on \>password +@admin
+redis-cli acl setuser default off
